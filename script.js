7a5219103951dab184fd2e7dfb3350d29a6172a9
Vamsi <kattavamsi603@gmail.com>
2024-02-11 15:10:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const accessKey = "jc-lNRObh2F0tqyzQlN9duYKxcJ_BGUZUe3G5ZHZwUU";
+
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMorebtn = document.getElementById("show-more-btn");
+
+let keyword = "";
+let page = 1;
+
+async function searchImages(){
+  keyword = searchBox.value;
+  const url =`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+  if(page === 1){
+    searchResult.innerHTML = "";
+  }
+
+  const results = data.results;
+  results.map((result) =>{
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+
+    imageLink.appendChild(image);
+    searchResult.appendChild(imageLink);
+  })
+  showMorebtn.style.display = "block";
+
+}
+
+searchForm.addEventListener("submit",(e) =>{
+  e.preventDefault();
+  page = 1;
+  searchImages();
+})
+
+showMorebtn.addEventListener("click",()=>{
+  page++;
+  searchImages();
+})
